cd82a0398e95011376886f79bf09e5b357893b4f
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-12-11 15:20:03 +0800

    component_title

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-function App() {
-  const name = "Jen"
-  const x = true
-  const y = false
+import Header from './components/Header'
 
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello From React</h1>
-      <h2>Hello { name }</h2>
-      <h2>Hello { 1 + 1 }</h2>
-      <h2>Hello { x? 'Yes' : 'No'}</h2>
-      <h2>Hello { y? 'Yes' : 'No' }</h2>
+    <div className='container'>
+      <Header title={1} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
